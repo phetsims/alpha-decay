7b2f0e831ae4b75fa5620e124f3be906c5eae8ef
AgustinVallejo <agustin.vallejo@udea.edu.co>
2026-04-02 12:05:57 -0300

    Implementing equation functionality for first screen

--- js/single-atom/view/ADSingleAtomScreenView.ts
+++ js/single-atom/view/ADSingleAtomScreenView.ts
@@ -9,6 +9,8 @@
 import Bounds2 from '../../../../dot/js/Bounds2.js';
 import NuclearDecayCommonConstants from '../../../../nuclear-decay-common/js/NuclearDecayCommonConstants.js';
 import EnergyDiagramAccordionBox from '../../../../nuclear-decay-common/js/view/EnergyDiagramAccordionBox.js';
+import EquationAccordionBox from '../../../../nuclear-decay-common/js/view/EquationAccordionBox.js';
+import ParticleCountsAccordionBox from '../../../../nuclear-decay-common/js/view/ParticleCountsAccordionBox.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
 import AlphaDecayScreenView, { AlphaDecayScreenViewOptions } from '../../common/view/AlphaDecayScreenView.js';
 import ADSingleAtomModel from '../model/ADSingleAtomModel.js';
@@ -22,19 +24,33 @@ export default class ADSingleAtomScreenView extends AlphaDecayScreenView {
 
   public constructor( model: ADSingleAtomModel, providedOptions: AlphaDecayScreenViewOptions ) {
 
-    const options = optionize<ADSingleAtomScreenViewOptions, SelfOptions, AlphaDecayScreenViewOptions>()( {
-    }, providedOptions );
+    const options = optionize<ADSingleAtomScreenViewOptions, SelfOptions, AlphaDecayScreenViewOptions>()( {}, providedOptions );
 
     super( model, options );
 
+    // Right contents panel
+    const particleCountsAccordionBox = new ParticleCountsAccordionBox( model, {
+      tandem: options.tandem.createTandem( 'particleCountsAccordionBox' )
+    } );
+    const equationAccordionBox = new EquationAccordionBox( model.selectedIsotopeProperty,
+      model.isPlayAreaEmptyProperty,
+      model.hasDecayOccurredProperty,
+      {
+        tandem: options.tandem.createTandem( 'equationAccordionBox' )
+      } );
+    this.rightColumnControls.addChild( particleCountsAccordionBox );
+    this.rightColumnControls.addChild( equationAccordionBox );
+
     // Bottom-left panel
 
-    const energyDiagramAccordionBox = new EnergyDiagramAccordionBox( {
-      minWidth: NuclearDecayCommonConstants.LONG_PANEL_WIDTH,
-      left: this.layoutBounds.minX + NuclearDecayCommonConstants.SCREEN_VIEW_X_MARGIN,
-      bottom: this.layoutBounds.maxY - NuclearDecayCommonConstants.SCREEN_VIEW_Y_MARGIN,
-      fill: NuclearDecayCommonConstants.MAIN_PANEL_FILL
-    } );
+    const energyDiagramAccordionBox = new EnergyDiagramAccordionBox(
+      model, this.modelViewTransformProperty,
+      {
+        minWidth: NuclearDecayCommonConstants.LONG_PANEL_WIDTH,
+        left: this.layoutBounds.minX + NuclearDecayCommonConstants.SCREEN_VIEW_X_MARGIN,
+        bottom: this.layoutBounds.maxY - NuclearDecayCommonConstants.SCREEN_VIEW_Y_MARGIN,
+        fill: NuclearDecayCommonConstants.MAIN_PANEL_FILL
+      } );
     this.addChild( energyDiagramAccordionBox );
 
     const playAreaBounds = new Bounds2(
